3cffcf10f59fd5df9a850a9d98f294220cb4fe1b
Ankur Prajapati <ankurbit1997@gmail.com>
2023-05-11 12:15:32 +0530

    branch build check

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import {View, Text} from 'react-native';
+import {Text, SafeAreaView} from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Hello from Dev</Text>
+    </SafeAreaView>
   );
 };
 
